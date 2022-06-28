cc0fb3fca504e7df02cff8e75425327d4ac04c43
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-28 14:34:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const foodList = [`uova`,`pane`,`latte`,`biscotti`,`pasta`,`affettati`,`formaggio`,`verdura`,`frutta`];
+const shoppingList = document.getElementById(`shoppinglist`);
+
+let foodItem = foodList[0];
+
+// for(let i = 1;i<=foodList.length;i++){
+//     foodItem += `<li>${foodList[i]}</li>`
+// }
+// console.log(foodItem);
+// shoppingList.innerHTML = `<li>${[foodItem]}</li>`
+let i = 1;
+while(i<foodList.length){
+    foodItem += `<li>${foodList[i]}</li>`
+    i++;
+}
+console.log(foodItem);
+shoppingList.innerHTML = `<li>${[foodItem]}</li>`;
